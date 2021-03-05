3e70dedab01109b07e9f82f46add057d28dcb1e0
luisfalconmx <me@luisfalconmx.com>
2021-03-04 22:16:05 -0600

    feat: browsersync added in the project for use like to live server

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const pug = require('gulp-pug')
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 gulp.task('compile:css', () => {
   return gulp
@@ -20,3 +22,14 @@ gulp.task('compile:pug', () => {
     }))
     .pipe(gulp.dest('./dist'))
 })
+
+gulp.task('server', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  });
+
+  gulp.watch('./src/styles/**/*.css', gulp.series(['compile:css'])).on('change', reload);
+  gulp.watch('./src/views/**/*.pug', gulp.series(['compile:pug'])).on('change', reload);
+});
