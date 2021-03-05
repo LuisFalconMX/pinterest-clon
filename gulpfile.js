fa72dcc854e9dfeb7b2dd3fac238a8b276ef4931
luisfalconmx <me@luisfalconmx.com>
2021-03-04 22:50:11 -0600

    feat: npm run dev command configured with gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const sugarss = require('sugarss')
 const pug = require('gulp-pug')
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+const clean = require('gulp-clean')
 
-gulp.task('compile:css', () => {
+gulp.task('compile:postcss', () => {
   return gulp
     .src('./src/styles/index.sss')
     .pipe(postcss([
@@ -21,11 +22,23 @@ gulp.task('compile:css', () => {
 
 gulp.task('compile:pug', () => {
   return gulp
-    .src('./src/views/*.pug')
+    .src('./src/views/pages/**/*.pug')
     .pipe(pug({
       pretty: true
     }))
     .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('create:dist', () => {
+  return gulp
+    .src('./')
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('clean:dist', () => {
+  return gulp
+    .src('./dist/')
+    .pipe(clean())
 })
 
 gulp.task('server', () => {
@@ -33,8 +46,10 @@ gulp.task('server', () => {
     server: {
       baseDir: './dist/'
     }
-  });
+  })
 
-  gulp.watch('./src/styles/**/*.css', gulp.series(['compile:css'])).on('change', reload);
-  gulp.watch('./src/views/**/*.pug', gulp.series(['compile:pug'])).on('change', reload);
-});
+  gulp.watch('./src/styles/**/*.css', gulp.series(['compile:postcss'])).on('change', reload)
+  gulp.watch('./src/views/**/*.pug', gulp.series(['compile:pug'])).on('change', reload)
+})
+
+gulp.task('dev', gulp.series(['create:dist', 'clean:dist', 'compile:postcss', 'compile:pug', 'server']))
